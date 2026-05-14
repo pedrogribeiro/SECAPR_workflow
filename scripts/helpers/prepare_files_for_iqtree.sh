4ab76cad7aaae4ef0776a334426a45fa59fcf85a
pedrogribeiro <44499715+pedrogribeiro@users.noreply.github.com>
2026-05-14 09:29:40 +0200

    Update prepare_files_for_iqtree.sh

--- scripts/helpers/prepare_files_for_iqtree.sh
+++ scripts/helpers/prepare_files_for_iqtree.sh
@@ -2,48 +2,29 @@
 # Purpose: prepare MAFFT-aligned locus files for downstream concatenation and IQ-TREE analysis.
 # Note: this script should be run in the directory containing the MAFFT output files.
 
-##### remove consensus sequence from fasta files #####
-for file in *.fasta; do
-    sed '/>P/{N;d;}' "./${file}" > "./${file%%.*}.fas"
-done
+# Transform fasta in one-line fastas, change file extension to .fas to avoid losing info
+for file in *.fasta; do awk 'NR==1 {printf("%s\n", $0); next} /^>/ {printf("\n%s\n",$0); next; } { printf("%s",$0);} END {printf("\n");}' "$file" > "${file%%.*}.fas"; done
 
-##### remove original fasta files #####
+# delete the unchanged fasta
 rm *.fasta
 
-##### remove empty lines #####
-sed -i '/^$/d' *.fas
+# Alignments come with headers (P1, P2, etc) that should not be in the concatenated aligment. Remove them
+for file in *.fasta; do sed '/>P/,/^/d' < "./""$file" > "./""${file%%.*}"".fasta"; done
 
-##### remove non-fasta files from subdirectories #####
-find ./*/ -type f ! -name '*.fasta' -delete
-
-##### remove empty lines from fasta files in subdirectories #####
-for file in */*.fasta; do
-    sed -i '/^\s*$/d' "./${file}"
-done
-
-##### remove empty files in subdirectories #####
+# In case the previous generates empty lines or files, delete those.
+for file in *.fasta; do sed -i '/^\s*$/d' "./""$file"; done
 find . -empty -type f -delete
 
-##### change voucher names #####
-# Note: only do this if really needed/you need to change sample names in alignments. If you did not use SECAPR for assembly it might not be needed
-#for file in *.fas; do
-#    seqkit replace -p "(.+)" -r '{kv}' -k ./taxon_list.txt "./${file}" > "./${file%%.*}.fasta"
-#done
+# Moving on to changing "-" for "N"
+for file in *.fasta; do sed -i '/^>/!y/-/N/' "./""$file"; done
 
-##### remove intermediate fas files #####
-rm *.fas
-
-##### change gap characters to N #####
-for file in *.fasta; do
-    sed -i '/^>/!y/-/N/' "./${file}"
-done
-
-##### add missing sequences with secapr #####
+# Concatenation does not allow missing sequences, so we need to add them even if they are supposed to be only "Ns". Secapr can do this.
 module load mambaforge
-mamba activate secapr_env
+mamba activate /storage/plzen1/home/pedroribeiro/Software/secapr_env
+mkdir secaprAddMissing
 secapr add_missing_sequences --input . --output ./secaprAddMissing
 
 ##### concatenate sequences for iqtree #####
 # Note: the partitions file can be used to generate a file that will be used in Partition Finder, if you use it. concatenated.phy is your final alignment. 
-cd ./secaprAddMissing || exit 1
-~/software/catfasta2phyml/catfasta2phyml.pl *.fasta > concatenated.phy 2> partitions.txt
+source ~/.bashrc
+catfasta2phyml.pl --sequential *.fasta > ButtConcatenated.phy 2> partitions.txt
